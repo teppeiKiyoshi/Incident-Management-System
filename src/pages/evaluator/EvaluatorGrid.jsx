cdb7abd535f7801e7450ff5b71d189e2fd209bf1
teppeiKiyoshi <martinemanuelleo@gmail.com>
2022-04-15 00:07:27 +0800

    added eval reg form modal

--- src/pages/evaluator/EvaluatorGrid.jsx
+++ src/pages/evaluator/EvaluatorGrid.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './evalGrid.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
@@ -7,8 +7,21 @@ import SortEvaluator from '../../components/dropdown/SortEvaluator';
 //MUI dependencies
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import TextField from '@mui/material/TextField';
 
 const EvaluatorGrid = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
   return (
     <div className='evalgrid-main'>
       <Sidebar />
@@ -21,12 +34,31 @@ const EvaluatorGrid = () => {
               startIcon={<AddIcon />}
               variant='contained'
               className='evalreg-btn'
+              onClick={toggleModal}
             >
               Register
             </Button>
           </div>
         </div>
         <div className='evalgrid-functions'>
+          {modal && (
+            <div className='modal'>
+              <div onClick={toggleModal} className='overlay'></div>
+              <div className='modal-content'>
+                <h2 className='content-title'>Register an Evaluator</h2>
+                <TextField required variant='outlined' label='Last Name'/>
+                <TextField required variant='outlined' label='First Name'/>
+                <TextField variant='outlined' label='M.I.' sx={{ width: '60px'}}/>
+                <TextField required fullWidth variant='outlined' label='Email'/>
+                <TextField required fullWidth variant='outlined' label='Password'/>
+                <button className="btn_cancel">Cancel</button>
+                <button className="btn_submit">Register</button>
+                <button className='close-modal' onClick={toggleModal}>
+                  X
+                </button>
+              </div>
+            </div>
+          )}
           <SortEvaluator />
         </div>
         <div className='card-container'>
